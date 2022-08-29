29f8367beda4092556e1153c4f849910a2e2f228
topra <topra@DESKTOP-JDQK8TU>
2022-08-29 10:31:27 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿namespace CharParser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length != 1) //Error handler for multiple or no input args
+            {
+                Console.WriteLine("Wrong number of arguments");
+                return;
+            }
+            int[] arr; //Static size letter counter initialization p1
+            arr = new int[27]; //p2: setting size
+            for (int i = 0; i < arr.Length; i++) //p3: set all elements of counter array to 0
+            {
+                arr[i] = 0;
+            }
+            for (int i = 0; i < args[0].Length; i++) //Scan every character in the given String
+            {
+                if ((args[0][i] > 122) || (args[0][i] < 97)) // 'a' to 'z' ASCII boundary set
+                {
+                    Console.WriteLine("Unexpected character in sequence");
+                    return;
+                }
+                arr[args[0][i] - 97] += 1; //Subtract ASCII of 'a' to shift sequence to [0 26], from [97 122]
+            }
+            for (int i = 0; i < arr.Length; i++) //Scan over array 27 times to ensure all letters are evaluated
+            {
+                int maxValue = arr.Max(); //Find maximum value in the array
+                int maxIndex = Array.IndexOf(arr, maxValue); //Find where maximum value can be found
+                if (maxValue > 0) //Disregard if given letter doesn't actually exist in the String
+                {
+                    Console.Write(Convert.ToChar(maxIndex + 97)); //Add 97 to re-shift value to proper ASCII equivalent, and display numerical ASCII as actual character
+                    arr[maxIndex] = 0; //Set occurance tracker to 0, to avoid re-evaluation of the same letter
+                }
+            }
+        }
+    }
+}
---
+++ README.txt
@@ -0,0 +1,24 @@
+﻿TASK:
+In this case, you will implement a console application that takes a string as an argument and returns a new string cleared of duplicates. The result should be ordered from the most repeated character to the least repeated character. If they have the same degree, sort them in alphabetical order.
+
+Execution:
+> ./app bcabaa
+> abc
+
+Sample-1:
+Input: bcabaa
+Output: abc
+Description: Input has 3 a, 2 b and 1 c. So, output should be a,b and c.
+
+Sample2: 
+Input: edceadec
+Output: ecda
+Description: Input has 3 e, 2 d, 2c and 1a. Since d and c have same degree, the correct order should be c and d.
+
+
+***************************************************************
+To run:
+
+Give Main method args using 
+		cd ......\CharParser\bin\Debug\net6.0 CharParser.exe <args>
+
